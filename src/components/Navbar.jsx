56fcdc20fab7daad3959ea0d8ee590f0dbb10cc3
saiyashwanth07 <saiyashwanth7386@gmail.com>
2026-05-27 17:02:27 +0530

    Updated to wide logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,8 +22,7 @@ const Navbar = () => {
     <nav className={`navbar ${isScrolled ? 'scrolled glass-panel' : ''}`}>
       <div className="container navbar-container">
         <Link to="/" className="navbar-logo" onClick={closeMenu}>
-          <img src="/guni-logo-red.png" alt="Ganpat Logo" style={{ height: '45px', width: '45px', borderRadius: '4px', marginRight: '10px', objectFit: 'contain' }} />
-          <span className="logo-text">Ganpat University</span>
+          <img src="/guni-logo-wide.png" alt="Ganpat Centre of Excellence Logo" style={{ height: '55px', width: 'auto', objectFit: 'contain' }} />
         </Link>
 
         <div className="desktop-menu">
